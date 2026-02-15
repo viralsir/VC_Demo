4e559c95971aa5742724b36b0ea42954a33ba0d0
Viral <viralsir2018@gmail.com>
2026-02-15 12:06:23 +0530

    Entry and View Logic has been completed successfully.

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
    define memeber variable and member function represent student
    and add Entry and view logic for at least one student.
@@ -5,5 +7,41 @@
  */
 public class Student
 {
+     int rollno;
+     String name;
+     int maths;
+     int science;
+
+
+     void setStudent(){
+         Scanner scanner=new Scanner(System.in);
+
+         System.out.println("Enter Student Roll No:");
+         rollno=scanner.nextInt();
+         System.out.println("Enter Student Name:");
+         name=scanner.next();
+         System.out.println("Enter Student Maths Marks:");
+         maths= scanner.nextInt();
+         System.out.println("Enter Student Science Marks:");
+         science=scanner.nextInt();
+
+     }
+
+     void getStudent(){
+         System.out.println("Roll No :"+rollno);
+         System.out.println("Name:"+name);
+         System.out.println("Maths :"+maths);
+         System.out.println("Science :"+science);
+     }
+
+    public static void main(String[] args) {
+          Student student=new Student();
+
+          student.setStudent();
+          student.getStudent();
+
+    }
+
+
 
 }
